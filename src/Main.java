200fd1bd538bc587e0ec9db3df3e03cdaa7134ca
miqdok <milana.horalevych@gmail.com>
2024-03-13 23:06:48 +0200

    Add input and search for a substring in a line, as well as output of the result

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // Зчитування N рядків
-        ArrayList<String> lines = readNLines();
+        // Зчитування підрядка
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the substring to be found: ");
+        String findingSubstring = scanner.nextLine();
 
-        // Виведення зчитаних рядків
-        System.out.println("Entered lines: ");
-        for (String line : lines) {
-            System.out.println(line);
+        // Перевірка, чи введено підрядок
+        if (!findingSubstring.isEmpty()) {
+            // Зчитування N рядків
+            ArrayList<String> lines = readNLines();
+
+            // Пошук входжень підрядка та виведення результатів
+            int[][] result = findingSubstring(lines, findingSubstring);
+            //System.out.println("Substring occurrences: ");
+            for (int i = 0; i < result.length; i++) {
+                System.out.println(result[i][0] + " occurrences in line " + result[i][1]);
+            }
+        } else {
+            System.out.println("An empty substring was entered");
         }
     }
 
@@ -36,4 +47,38 @@ public class Main {
         }
         return lines;
     }
+
+    // Метод для пошуку входжень підрядка у кожному рядку
+    private static int[][] findingSubstring(ArrayList<String> lines, String substring) {
+        int[][] result = new int[lines.size()][2];
+
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+            int count = countSubstring(line, substring);
+            result[i][0] = count;
+            result[i][1] = i;
+        }
+        return result;
+    }
+
+    // Метод, що рахує входження підрядка
+    private static int countSubstring(String line, String substring) {
+        int count = 0;
+        int lengthOfSubstring = substring.length();
+        int lengthOfLine = line.length();
+
+        // Проходимося по рядку
+        for (int i = 0; i <= lengthOfLine - lengthOfSubstring; i++) {
+            // Витягуємо підрядок довжиною підрядка для порівняння
+            String sub = line.substring(i, i + lengthOfSubstring);
+
+            // Порівнюємо підрядок з шуканим підрядком
+            if (sub.equals(substring)) {
+                count++;
+                i += lengthOfSubstring - 1;
+            }
+        }
+        return count;
+    }
+
 }
