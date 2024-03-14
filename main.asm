9b32825660d52658026b89c68aa8f8df78aa9d3c
miqdok <milana.horalevych@gmail.com>
2024-03-14 22:34:39 +0200

    Add substring reading and output

--- main.asm
+++ main.asm
@@ -2,12 +2,50 @@
 .stack 100h
 
 .data
+substring db 255 dup(0)                  ; Змінна для зберігання командного рядка
+substringLength db ?                     ; Змінна для зберігання довжини командного рядка
 oneChar db ?                             ; Змінна для зберігання одного символу
 
 .code
 main PROC
-    mov ax, @data                        ; Initialize data segment
+    mov ax, ds
+    mov es, ax                           ; Збереження сегменту PSP в ES для доступу до аргументів командного рядка
+    
+    mov ax, @data                        ; Ініціалізування сегменту даних
     mov ds, ax
+
+    xor ah, ah
+    mov al, es:[80h]                     
+    mov substringLength, al              ; Збереження довжини аргументу командного рядка
+
+    mov si, 82h                          ; Початок аргументу командного рядка
+    mov di, offset substring             ; Призначення(місце) аргументу командного рядка
+    mov cl, substringLength
+
+copy_substring:
+    mov al, es:[si]                      ; Копіювання аргументу командного рядка до параметра 
+    mov [di], al                         ; Копіювання аргументу до масиву параметрів
+    inc si
+    inc di
+    dec cl
+    cmp cl, 0
+    jne copy_substring                   ; Повторення циклу, поки не буде скопійовано всі аргументи
+
+    xor ax, ax
+    mov di, offset substring
+    mov cl, substringLength
+print_substring:
+    mov ah, 02h                          ; Функція виводу символу
+    mov dl, [di]                         ; Завантаження символу для виводу
+    int 21h                              ; Виклик DOS interrupt
+    inc di
+    dec cl
+    cmp cl, 0
+    jne print_substring
+
+    mov dl, 0Ah                          ; Новий рядок
+    int 21h
+
 read_next:
     mov ah, 3Fh                          ; Зчитування символа з stdin
     mov bx, 0h                           ; stdin handle
